ed7f72fd02bf3bc864b8a9734611979dfa278999
iyama65 <33947927+iyama65@users.noreply.github.com>
2017-12-15 13:36:52 +0900

    header

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+    var posX = [];
+    var posY = [];
+    var speedX = [];
+    var speedY = [];
+    var diameter = []; //円の直径
+    var colors = []; //色
+    var NUM = 100; //配列の数
+    var img;
+
+    function preload(){
+        img = loadImage("img/main.jpg");
+    }
+
+    function setup(){
+        // size
+        var canvas = createCanvas(windowWidth,600);
+        // header#headerの中にcanvasを入れた
+        canvas.parent('header');
+
+        colorMode(HSB,360,100,100,100);
+
+        for(var i = 0; i < NUM; i++){
+            diameter[i] = random(15,60);
+            colors[i] = color(random(360),100,100,30);
+            posX[i] = random(width);
+            speedX[i] = random(-4,4);
+            posY[i] = random(height);
+            speedY[i] = random(-4,4);
+        }
+
+    }
+
+    function draw(){
+        background(0);
+        image(img,0,0);
+        noStroke();
+
+        for(var i=0; i <NUM; i++){
+            fill(colors[i]);
+            ellipse(posX[i],posY[i],diameter[i],diameter[i]);
+            posX[i] += speedX[i];
+
+            if (posX[i] > width || posX[i] < 0){
+                speedX[i] = -1 * speedX[i];
+            }
+
+            posY[i] += speedY[i];
+            if (posY[i] > height || posY[i] < 0){
+                speedY[i] = -1 * speedY[i];
+            }
+        }
+
+    }
+
+    function windowResized(){
+        resizeCanvas(windowWidth,700);
+    }
